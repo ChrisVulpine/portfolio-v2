b87bafaa4df8eb3d05c1a8431ba10ba45386c334
Chris <157438747+ChrisVulpine@users.noreply.github.com>
2024-05-31 02:57:48 -0400

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,61 @@
+// Nav hamburgerburger selections
+const burger = document.querySelector("#burger-menu");
+const ul = document.querySelector("nav ul");
+const nav = document.querySelector("nav");
+
+// Scroll to top selection
+const scrollUp = document.querySelector("#scroll-up");
+
+// Select nav links
+const navLink = document.querySelectorAll(".nav-link");
+
+// Hamburger menu function
+burger.addEventListener("click", () => {
+  ul.classList.toggle("show");
+});
+
+// Close hamburger menu when a link is clicked
+navLink.forEach((link) =>
+  link.addEventListener("click", () => {
+    ul.classList.remove("show");
+  })
+);
+
+// scroll to top functionality
+scrollUp.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: "smooth",
+  });
+});
+
+// Function to check if an element is in viewport
+function isInViewport(element) {
+  var rect = element.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
+
+// Function to handle scroll event
+function fadeSections() {
+  var sections = document.querySelectorAll('.fade-in-section');
+  
+  sections.forEach(function(section) {
+    if (isInViewport(section)) {
+      section.classList.add('fade-in');
+    } else {
+     
+    }
+  });
+}
+
+// Event listener for scroll event
+window.addEventListener('scroll', fadeSections);
+
+// Initial check on page load
+fadeSections();
